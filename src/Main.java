7ff980319ef6adac6f2230d6fe1c0cee6a339047
Andrey Alexandrovich <ArkhipovAndrei73@gmail.com>
2023-08-07 15:31:07 +0400

    Вот

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,15 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int number = (int) (Math.random() * 100);
         String url = "http://numbersapi.com/" + number + "/trivia";
-        String response = null;
-        try {
-            response = sendGetRequest(url);
-            System.out.println("Response: " + response);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        int[] countSymbol = countSymbol(response);
-        char[] ch = response.toCharArray();
-        Set<Character> chars = new HashSet<>();
-        for (char c : ch) {
-            chars.add(c);
-        }
-        for (char cha: chars) {
-            System.out.println(cha + " " + countSymbol[cha] + " раз");
-        }
-    }
-
-
-    private static String sendGetRequest(String url) throws IOException {
-
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-        return response.toString();
-    }
-
-    private static int[] countSymbol(String str) {
-        int[] charCount = new int[256];
-
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            charCount[ch]++;
-        }
-
-        return charCount;
+        TextService textService = new TextService();
+        String response = textService.sendGetRequest(url);
+        TextUtils textUtils = new TextUtils();
+        Map<Character, Integer> charCountMap = textUtils.countChars(response);
+        System.out.println(response);
+        System.out.println(charCountMap);
     }
 }
